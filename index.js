817cb4264d80a848e8f86180e025522d45061287
Sergio N <s.navarroredondo@gmail.com>
2025-11-02 15:02:39 +0100

    REMOVE unused imports from microservices-template

--- index.js
+++ index.js
@@ -1,23 +1,13 @@
 import  express from 'express'
 import { createServer } from 'http';
 import { globalErrorHandler } from './middleware/globalErrorHandler.js';
-import { connectToMessageBroker, publishDirect, publishLogs } from './config/messageBroker/LavinMQ.js';
+import { connectToMessageBroker } from './config/messageBroker/LavinMQ.js';
 
 
 const app = express();
 const server = createServer(app);
 
 connectToMessageBroker();
-
-
-setTimeout(() => { //* Reminder to allow a few seconds for the message broker to connect before trying to send a message 
-
-  publishDirect('{"text":"hello", "id": "123", "type": "case1"}')
-
-  publishLogs('{"timestamp": "2025-11-01T16:00:00Z", "service": "transcription-service", "level": "INFO", "message": "exampleMessage", "event":"exampleEvent", "environment": "production"}')  
-  // Optional "userId": "123456789",
-
-}, 1000)
 
 //* Middleware to catch & handle errors
 app.use(globalErrorHandler);
@@ -28,7 +18,6 @@ app.listen(process.env.PORT, () => {
 
 const gracefulShutdown = () => {
     console.log('Received shutdown signal, closing server...');
-    console.log('LavinMQ connection closed'); // TODO -- add graceful disconnection from LavinMQ
 
     server.close(() => {
     console.log('Express server closed');
