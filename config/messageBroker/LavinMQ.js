945b9ef3a541df22d49b7ad782480990e78f6bd4
Sergio N <s.navarroredondo@gmail.com>
2025-11-06 00:10:21 +0100

    CLEANUP lavinMQ implementation
    Reorganize queues declarations & bindings
    
    Add helper functions for debugging
    
    refactor logic into separate files to easier handling

--- config/messageBroker/LavinMQ.js
+++ config/messageBroker/LavinMQ.js
@@ -1,28 +1,26 @@
-import { AMQPClient } from '@cloudamqp/amqp-client'
+import { AMQPClient } from '@cloudamqp/amqp-client';
 import { handleTranscriptionRequestedQueue } from './transcriptionService.js';
 
 let connection;
 let channel;
-let transcriptionRequestedQueue
-let transcriptionCompletedQueue
-let insightsRequestedQueue
-let logsQueue
-
+let transcriptionRequestedQueue;
+let transcriptionCompletedQueue;
+let logsQueue;
 
 const startConsumers = async () => {
   try {
     const transcriptionRequestedConsumer = await transcriptionRequestedQueue.subscribe({ noAck: false }, async (msg) => {
-        try {
-          await handleTranscriptionRequestedQueue(msg)
-          await msg.ack();
-        } catch (error) {
-          console.log('error processing message', error)
-          await msg.nack(true); // Requeue on failure
-          // await msg.nack(true); //* NOT Requeue on failure - For debugging and avoiding infite loops
-        }
+      try {
+        await handleTranscriptionRequestedQueue(msg);
+        await msg.ack();
+      } catch (error) {
+        console.log('error processing message', error);
+        await msg.nack(true); // Requeue on failure
+        // await msg.nack(true); //* NOT Requeue on failure - For debugging and avoiding infite loops
+      }
     });
 
-    console.log('consumers started successfully')
+    console.log('consumers started successfully');
   } catch (err) {
     console.log(err);
   }
@@ -32,7 +30,7 @@ const startConsumers = async () => {
 export const connectToMessageBroker = async () => {
   try {
     // 1. Establish connection
-    const amqp = new AMQPClient(process.env.LAVINMQ_HOST); 
+    const amqp = new AMQPClient(process.env.LAVINMQ_HOST);
     connection = await amqp.connect(); // Establish connection to the message broker - one connection for all channels
 
     // 2. Open producer and consumer channels
@@ -40,115 +38,105 @@ export const connectToMessageBroker = async () => {
 
     // 3. Declare exchanges & queues & bindings
 
-      // 3.1 declare exchanges
+    // 3.1 declare exchanges
 
-        const logsExchange = await channel.exchangeDeclare('logs_exchange', 'topic', {
-            durable: true,
-            passive: false,
-            autoDelete: false,
-            internal: false,
-          });
+    const logsExchange = await channel.exchangeDeclare('logs_exchange', 'topic', {
+      durable: true,
+      passive: false,
+      autoDelete: false,
+      internal: false,
+    });
 
-        
-          const analysisExchange = await channel.exchangeDeclare('analysis_exchange', 'topic' , {
-              durable: true,
-              passive: false,
-              autoDelete: false,
-              internal: false,
-            })
+    const analysisExchange = await channel.exchangeDeclare('analysis_exchange', 'topic', {
+      durable: true,
+      passive: false,
+      autoDelete: false,
+      internal: false,
+    });
 
-        // 3.2 declare queues
+    // 3.2 declare queues
 
-        logsQueue = await channel.queue('logs_queue', {
-            durable: true,
-            passive: false,
-            autoDelete: false,
-            exclusive: false,
-          });
+    logsQueue = await channel.queue('logs_queue', {
+      durable: true,
+      passive: false,
+      autoDelete: false,
+      exclusive: false,
+    });
 
-        transcriptionRequestedQueue = await channel.queue('transcription_requested_queue', {
-            durable: true,
-            passive: false,
-            autoDelete: false,
-            exclusive: false,
-          });
+    transcriptionRequestedQueue = await channel.queue('transcription_requested_queue', {
+      durable: true,
+      passive: false,
+      autoDelete: false,
+      exclusive: false,
+    });
 
-        transcriptionCompletedQueue = await channel.queue('transcription_completed_queue', {
-            durable: true,
-            passive: false,
-            autoDelete: false,
-            exclusive: false,
-          });
+    transcriptionCompletedQueue = await channel.queue('transcription_completed_queue', {
+      durable: true,
+      passive: false,
+      autoDelete: false,
+      exclusive: false,
+    });
 
-        insightsRequestedQueue = await channel.queue('insights_requested_queue', {
-            durable: true,
-            passive: false,
-            autoDelete: false,
-            exclusive: false,
-          });
+    // 3.3 Bind queues to exchange with routing key
+    await transcriptionRequestedQueue.bind('analysis_exchange', 'analysis.analysisEntry.transcription.requested', {
+    });
 
-          // 3.3 Bind queues to exchange with routing key
-        await transcriptionRequestedQueue.bind('analysis_exchange', 'analysis.analysisEntry.transcription.requested', {
-          });
+    await transcriptionCompletedQueue.bind('analysis_exchange', 'analysis.analysisEntry.transcription.completed', {
+    });
 
-        await transcriptionCompletedQueue.bind('analysis_exchange', 'analysis.analysisEntry.transcription.completed', {
-          });
+    await logsQueue.bind('logs_exchange', 'logs.#', {
+      // no args
+    });
 
-        await insightsRequestedQueue.bind('analysis_exchange', 'analysis.analysisEntry.insights.requested', {
-          });
-
-        await logsQueue.bind('logs_exchange', "logs.#", {
-        // no args
-        });
-    
-  // 4. Set up consumer/s
+    // 4. Set up consumer/s
 
     await startConsumers();
-    
+
     // await publishDebuggingCallToTranscriptionRequestedQueue() //* uncomment to send an example message to transcriptionRequestedQueue
-          
-    
-    console.log('transcription service successfully connected to LavinMQ message broker')
-    return { connection: connection, channel: channel, analysisExchange, transcriptionRequestedQueue: transcriptionRequestedQueue };
+
+    console.log('transcription service successfully connected to LavinMQ message broker');
+    return {
+      connection: connection, channel: channel, analysisExchange, transcriptionRequestedQueue: transcriptionRequestedQueue,
+    };
   } catch (e) {
-    console.error("ERROR", e);
+    console.error('ERROR', e);
     e.connection?.close();
     setTimeout(connectToMessageBroker, 1000); // will try to reconnect in 1s
   }
-}
+};
 
 // function for publishing to logs queue
 export const publishLogs = async (message) => {
-  console.log(`publishing to logsQueue message function called`);
+  console.log('publishing to logsQueue message function called');
   try {
     await logsQueue.publish(message);
   } catch (err) {
     console.error('Error publishing logs message:', err);
   }
-}
+};
 
-export const publishToInsightsRequestedQueue = async (message) => {
-  console.log(`publishing to insightsQueue message function called`);
+export const publishTranscriptionCompletedQueue = async (message) => {
+  console.log('publishing to transcriptionCompletedQueue message function called');
   try {
-    await insightsRequestedQueue.publish(message);
+    await transcriptionCompletedQueue.publish(message);
   } catch (err) {
-    console.error('Error publishing insights message:', err);
+    console.error('Error publishing transcription completed message:', err);
   }
-}
+};
 
 const publishDebuggingCallToTranscriptionRequestedQueue = async () => {
   const message = {
-              analysisEntryId: '4179f2eb-2405-44f5-a86d-d15c1d21eb5b',
-              analysisId: '70744eb2-f265-4713-959c-4dbeecabe901',
-              timestamp: new Date().toISOString(),
-              mediaType: 'video',
-              languageCode: 'es-ES',
-              outputBucket: 'dev-analysis-entry-storage',
-          };
+    analysisEntryId: '4179f2eb-2405-44f5-a86d-d15c1d21eb5b',
+    analysisId: '70744eb2-f265-4713-959c-4dbeecabe901',
+    timestamp: new Date().toISOString(),
+    mediaType: 'video',
+    languageCode: 'es-ES',
+    outputBucket: 'dev-analysis-entry-storage',
+  };
 
-          const stringifiedMessage = JSON.stringify(message);
+  const stringifiedMessage = JSON.stringify(message);
 
-          setTimeout(() => {
-            transcriptionRequestedQueue.publish(stringifiedMessage)
-          }, 5000);  
-}
+  setTimeout(() => {
+    transcriptionRequestedQueue.publish(stringifiedMessage);
+  }, 5000);
+};
