0639621eece6f722b1717fc072b0c514d6368192
Sergio N <s.navarroredondo@gmail.com>
2025-11-06 12:21:22 +0100

    ADD mongoDB npm package

--- config/messageBroker/transcriptionService.js
+++ config/messageBroker/transcriptionService.js
@@ -1,22 +1,38 @@
-import { requestAnalysisEntryTranscription } from '../../integrations/AWS/transcriptionJob.js';
-
+import { requestAnalysisEntryTranscription, startTranscriptionPolling } from '../../integrations/AWS/transcriptionJob.js';
+import { publishTranscriptionCompletedQueue } from './LavinMQ.js';
 
 export const handleTranscriptionRequestedQueue = async (msg) => {
-        const contentStr = msg.bodyToString();
+  const contentStr = msg.bodyToString();
+  const transcriptionRequest = JSON.parse(contentStr);
 
-        let transcriptionRequest;
+  switch (transcriptionRequest.mediaType) {
+    case 'video':
+      // console.log('Processing video transcription message') //* Debug
+      const transcriptionRequest = await requestAnalysisEntryTranscription(transcriptionRequest);
 
-        transcriptionRequest = JSON.parse(contentStr);        
+      const jobStatus = await startTranscriptionPolling('transcriptionJob');
 
-    switch (transcriptionRequest.mediaType) {
-        case 'video':
-            // console.log('Processing video transcription message') //* Debug
-            console.log(transcriptionRequest.outputBucket)
-            await requestAnalysisEntryTranscription(transcriptionRequest)
-        break;
+      if (jobStatus.status === 'COMPLETED') {
+        
+        const message = { // TODO -- fix message sent to transcriptionCompletedQueue - Probably will need to send analysisId and analysisEntryId so that the main service can fetch the transcription.json and store the transcript
+        };
 
-        default:
-            console.log(`[⚠️] Unknown media type: ${transcriptionRequest.mediaType}`);
-        break;
-        }
-}
+        const stringifiedMessage = JSON.stringify(message);
+
+        await publishTranscriptionCompletedQueue(stringifiedMessage);
+      }
+
+      else if (jobStatus.status === 'FAILED') {
+        console.error(`[⚠️] Transcription job failed for transcriptionId: ${transcriptionRequest.transcriptionId}`);
+      }
+      break;
+
+    case 'audio': // Prepare for the future, if needed
+      // console.log('Processing audio transcription message') //* Debug
+      break;
+
+    default:
+      console.log(`[⚠️] Unknown media type: ${transcriptionRequest.mediaType}`);
+      break;
+  }
+};
