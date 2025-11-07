4319f113fc318a43cc97135f5863d9a32180aa17
Sergio N <s.navarroredondo@gmail.com>
2025-11-07 15:23:23 +0100

    WIP 2 to fetch transcription.json from AWS S3 and parse it

--- controllers/videoTranscriptionController.js
+++ controllers/videoTranscriptionController.js
@@ -1,6 +1,7 @@
 import { publishToTranscriptionCompletedQueue } from '../config/messageBroker/LavinMQ.js';
 import { getS3Object } from '../integrations/AWS/S3.js';
 import { listCompletedTranscriptionJobsFromAWS, requestAnalysisEntryTranscription } from '../integrations/AWS/Transcribe.js';
+import { updateSingleVideoTranscriptRequestInDb, getTranscriptionJobsDetailsFromDb } from '../models/videoTranscriptionModel.js';
 
 export const handleCompletedVideoTranscriptionJobs = async () => {
   try {
@@ -11,7 +12,6 @@ export const handleCompletedVideoTranscriptionJobs = async () => {
     for (let i = 0; i < completedTranscriptionJobsSummary.length; i + 1) {
       const transcriptionJob = completedTranscriptionJobsSummary[i];
 
-
       try {
         // 1. Fetch JSON from AWS using transcription info (e.g., _id)
         // const jsonData = await fetchJsonFromAWS(transcription._id);
@@ -20,9 +20,19 @@ export const handleCompletedVideoTranscriptionJobs = async () => {
 
         const key = `analysis/${transcriptionJobDetails.analysisId}/${transcriptionJobDetails.analysisEntryId}/transcription.json`;
 
-        const jsonTranscription = await getS3Object(key);
+        const transcriptionFile = await getS3Object(key);
+
+        const jsonTranscription = await transcriptionFile.json();
 
         console.log(jsonTranscription);
+        // 2. Parse the JSON
+        // const parsedData = JSON.parse(jsonTrasncription);
+
+        // 3. Store parsed data in DB and update status value
+        // await storeInDB(parsedData);
+
+        // 4. Send parsed data to event queue
+        // await sendToEventQueue(parsedData);
       } catch (error) {
         console.error(`Error processing transcription ${transcriptionJob._id}:`, error);
         // Continue to next item
@@ -47,3 +57,21 @@ export const transcriptAnalysisEntry = async (transcriptionRequest, videoTranscr
 
   await updateSingleVideoTranscriptRequestInDb(videoTranscriptionRequestInsertId);
 };
+
+/*
+
+what i need to do is fetch the transcriptions, parse them, store them in DB and send them to the queue -- the question is - Where should each part of the process happen?
+
+1- check completed transcriptionJobs --> as a cron job
+2- update the status of the completed jobs in DB --> 
+3- fetch the transcriptions & parse them & store them in DB -->
+4- send them to the queue
+
+6- cron job to delete already process jobs from AWS
+
+el controller es el que deberia 
+1- actualizar la BBDD
+2- guardar la transcripcion en la BBDD
+3- llamar a la cola
+
+*/
