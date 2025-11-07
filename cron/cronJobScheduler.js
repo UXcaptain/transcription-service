fd16308abb3b7dcbd0802cae38d1028374805b2b
Sergio N <s.navarroredondo@gmail.com>
2025-11-07 12:47:19 +0100

    REMOVE implementation of  transcripcionRequestCompleted cron job

--- cron/cronJobScheduler.js
+++ cron/cronJobScheduler.js
@@ -1,10 +1,8 @@
 import { getCompletedTranscriptionJobsScheduler } from './getCompletedTranscriptionJobsScheduler.js';
-import { sendToTranscriptionCompletedQueue } from './sendToTranscriptionCompletedQueue.js';
 
 export const startCronJobs = () => {
   try {
     getCompletedTranscriptionJobsScheduler.start();
-    sendToTranscriptionCompletedQueue.start();
 
     console.log('Cron jobs started');
   } catch (error) {
