0394d43207b1ad55d8fb3cceef06ae10341a245c
Sergio N <s.navarroredondo@gmail.com>
2025-11-08 01:00:01 +0100

    FIX db connection not being persisted properly

--- database/config.js
+++ database/config.js
@@ -3,24 +3,23 @@ import { MongoClient } from 'mongodb';
 // Connection URL
 const url = process.env.MONGODB_URI;
 const client = new MongoClient(url);
+let db;
 
 export const connectToMongoDB = async () => {
-  let db;
-
-  if (!db) {
-    try {
-      await client.connect();
-
-      const transcriptionServiceDb = client.db('transcriptionServiceDB');
-
-      console.log('Connected successfully to mongodb'); // TODO - check why this is being called everytime
-
-      return transcriptionServiceDb;
-    } catch (error) {
-      console.error('Error connecting to MongoDB:', error);
-      throw error;
-    }
+  if (db) {
+    return db;
   }
 
-  return db;
+  try {
+    await client.connect();
+
+    db = client.db('transcriptionServiceDB');
+
+    console.log('Connected successfully to mongodb');
+
+    return db;
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+    throw error;
+  }
 };
