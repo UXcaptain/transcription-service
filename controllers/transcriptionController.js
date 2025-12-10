f4e20719d899a59bb8e3c65e534c6b8beb16dc49
Sergio N <s.navarroredondo@gmail.com>
2025-12-10 22:31:52 +0100

    WIP in handling completed transcripts

--- controllers/transcriptionController.js
+++ controllers/transcriptionController.js
@@ -1,9 +1,14 @@
 import { publishToTranscriptionCompletedQueue } from '../config/messageBroker/LavinMQ.js';
 import { getS3Object } from '../integrations/AWS/S3.js';
-import { deleteCompletedTranscriptionJobsFromAWS, listCompletedTranscriptionJobsFromAWS, requestAnalysisEntryTranscription } from '../integrations/AWS/Transcribe.js';
 import {
-  updateSingleTranscriptionRequestInDb, getSingleTranscriptionJobDetailsFromDb, storeParsedTranscriptionInDb, markTranscriptionAsPublishedToQueue,
+  deleteCompletedTranscriptionJobFromAWS,
+  listCompletedTranscriptionJobsFromAWS, requestAnalysisEntryTranscriptionToAWSTranscribe,
+} from '../integrations/AWS/Transcribe.js';
+import {
+  updateSingleTranscriptionRequestInDb,
+  getSingleTranscriptionJobDetailsFromDb,
   storeNormalizedTranscriptionInDb,
+  markTranscriptionAsPublishedToQueue,
 } from '../models/transcriptionModel.js';
 import { normalizeTranscript } from '../utils/transcriptionJobDataNormalizer.js';
 
@@ -12,10 +17,8 @@ export const handleCompletedVideoTranscriptionJobs = async () => {
     // Get the completed Jobs from AWS Transcribe
     const completedTranscriptionJobsSummary = await listCompletedTranscriptionJobsFromAWS();
 
-    console.log(completedTranscriptionJobsSummary);
-
     if (completedTranscriptionJobsSummary.length === 0) {
-      console.log('no completed entries to process');
+      console.log('no completed transcription jobs available to process');
     } else {
     // Iterate over every item
       for (let i = 0; i < completedTranscriptionJobsSummary.length; i + 1) {
@@ -23,36 +26,46 @@ export const handleCompletedVideoTranscriptionJobs = async () => {
 
         try {
         // 1. Get transcription job details from database
-          const transcriptionJobDetails = await getSingleTranscriptionJobDetailsFromDb(transcriptionJob.TranscriptionJobName);
+          const transcriptionJobDetails = await getSingleTranscriptionJobDetailsFromDb(transcriptionJob.TranscriptionJobName); //* Need this step for later usage of analysis details
 
           if (!transcriptionJobDetails) {
             throw Error(`job ${transcriptionJob.TranscriptionJobName} details dont exist in DB`);
           }
 
-          // Skip if already processed - Shouldnt happen too often if AWS Transcribe job deletion CRON is working properly
-          if (transcriptionJobDetails.publishedToQueue) {
+          // Skip if already processed - Shouldnt happen if AWS Transcribe job deletion is working properly
+          if (transcriptionJobDetails.publishedToQueue) { // ? What should the condition be?
             console.log(`Skipping already processed job: ${transcriptionJob.TranscriptionJobName}`);
-            return;
+
+          //   try { // * Delete the transcription job from AWS Transcribe since it has already been completed/published
+          //     await deleteCompletedTranscriptionJobFromAWS(transcriptionJob.TranscriptionJobName);
+          //   } catch (error) {
+          //     console.log(`failed to delete transcription job ${transcriptionJob.TranscriptionJobName}`, error);
+          //   }
+          //   return;
           }
 
+          /*
+            ? what im trying to do here is to see if i should process the transcription job or not? maybe it makes sense to reconcile via cron jobs or directly check for both completion and queue publishing conditions?
+          */
+
           // 2. Construct S3 key and fetch transcription file from AWS
-          const key = `analysis/${transcriptionJobDetails.analysisId}/${transcriptionJobDetails._id}/transcription.json`;
+          // const key = `analysis/${transcriptionJobDetails.analysisId}/${transcriptionJobDetails._id}/transcription.json`;
+          const key = 'analysis/464d4419-3822-41e6-8d8e-27b1783632df/eabd3179-ece0-4163-8c7e-0e2c728e11e6/transcription.json'; //* Debug - Point to the same transcription always
 
-          const transcriptionJobDataStructure = await getS3Object(key);
+          const transcriptionJobResult = await getS3Object(key);
 
-          // 3. Parse the transcription data structure
-          const parsedTranscriptionJobDataStructure = JSON.parse(transcriptionJobDataStructure);
+          // 3. Normalize transcription job result
 
-          const normalizedTranscriptionJobDataStructure = await normalizeTranscript(transcriptionJobDataStructure);
+          const normalizedTranscriptionJob = await normalizeTranscript(transcriptionJobResult);
 
           // 4. Store normalized transcript in DB and update status to COMPLETED
           await storeNormalizedTranscriptionInDb(transcriptionJob.TranscriptionJobName, normalizedTranscriptionJob);
 
-          try {
-            await deleteCompletedTranscriptionJobFromAWS(transcriptionJob.TranscriptionJobName);
-          } catch (error) {
-            console.log(`failed to delete transcription job ${transcriptionJob.TranscriptionJobName}`, error);
-          }
+          // try {
+          //   await deleteCompletedTranscriptionJobFromAWS(transcriptionJob.TranscriptionJobName);
+          // } catch (error) {
+          //   console.log(`failed to delete transcription job ${transcriptionJob.TranscriptionJobName}`, error);
+          // }
 
           // 5. Send normalized transcript to event queue
 
